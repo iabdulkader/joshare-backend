19727f59b9a79ef238500e5140bbdad2d9e67da2
iAbdulKader <abdulkadernow@gmail.com>
2022-02-17 00:35:33 +0600

    Major Fix

--- controllers/private/deleteFile.js
+++ controllers/private/deleteFile.js
@@ -4,7 +4,31 @@ const deleteFile = async (req, res) => {
   let { id } = req.params;
   let { pin } = req.user;
   
-  res.json({hello: "world"})
+  try {
+    const response = await User.findOneAndUpdate({ pin: pin }, {
+      $pull: {
+        files: {id: id},
+      }
+    }, { new: true })
+    
+    if(response){
+      res.json({
+        success: true,
+        msg: "Successfully Deleted"
+      })
+    } else {
+      res.json({
+        success: false,
+        msg: "Failed To Delete"
+      })
+    }
+  } catch (e) {
+    console.log(e.message)
+    res.json({
+      success: false,
+      msg: e.message
+    })
+  }
 }
 
 module.exports = {
